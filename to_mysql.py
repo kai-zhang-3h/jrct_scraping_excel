fee8cdac259649aa2d174e8b6cf723b334c174fa
root <root@3H230045.group-m3.com>
2024-11-26 18:28:06 +0900

    Use update if a record exists other wise insert

--- to_mysql.py
+++ to_mysql.py
@@ -2,7 +2,7 @@ import mysql.connector
 import os
 import openpyxl
 
-fieldnames = None
+names_jp = None
 rows = []
 
 # エクセルファイルの取り込み
@@ -12,33 +12,139 @@ ws = wb["Sheet1"]
 for row in ws.rows:
     if row[0].row == 1:
         # １行目
-        fieldnames = list(map(lambda x : x.value, row))
+        names_jp = list(map(lambda x : x.value, row))
     else:
         # ２行目以降
-        rows.append(tuple(list(map(lambda x : x.value, row))))
+        rows.append(list(map(lambda x : x.value, row)))
 
-# print(rows)
-print(fieldnames)
+def process_date(date):
 
-# ['初回公表日', '最終公表日', '実施期間（終了日）', '研究の種別', 
-# '治験の区分', '責任研究者の組織名', '他施設の責任研究者の組織名', 
-# '試験のフェーズ', '対象疾患名', '医薬品の一般名称', '販売名', 
-# '研究資金等の提供組織名称', '依頼者の名称', 
-# '他の臨床研究登録期間発行の研究番号', '試験進捗状況', 'JRCT_ID', 
-# '研究名称', '組入れ開始日', '試験概要の目標症例数', 
-# '試験概要の試験のタイプ', '試験問い合わせ窓口のE-mail', 
-# '試験問い合わせ窓口の担当者', 'url', '平易な研究名称']
+    if (date == None or date == ""): return date
 
-name_en = ['date_public_first', 'date_public_final', 'date_end', 'research_type', 'ct_filter',
-           'researcher_org', 'other_researcher_org', 'ct_phase', 'disease_name', 'medicine_name_general', 'medicine_name_sale',
-           'funding_org', 'dependence_name', 'research_num', 'ct_progression', 'jrctid',
-           'research_name', 'date_arrangement_start', 'num_target_disease', 'abstract_ct_type', 'inquiry_email',
-           'inquiry_window', 'url', 'research_name_simple']
+    def wrap_date(s):
+        y = s.split("年")[0]
+        m = s.split("年")[1].split("月")[0]
+        d = s.split("年")[1].split("月")[1].split("日")[0]
+
+        return y + "/" + m + "/" + d
+
+    if ("令和" not in date and "平成" not in date): 
+        return wrap_date(date)
+
+    hd = date.split("年")[0]
+    tl = date.split("年")[1]
+
+    y = None
+
+    if ("令和" in hd):
+        if "元" in hd:
+            y = 2019
+        else:
+            y = 2019 + int(hd.split("令和")[1]) - 1
+
+    elif ("平成" in hd): 
+        y = 1989 + int(hd.split("平成")[1]) - 1
+
+    return wrap_date(str(y) + "年" + tl)
+
+def process_research_type(research_type):
+    if (research_type == None or research_type == ""): return research_type
+    if (research_type == "企業治験"): return 1
+    elif (research_type == "医師主導治験"): return 2
+        
+def process_ct_filter(ct_filter):
+    if (ct_filter == None or ct_filter == ""): return ct_filter
+    if (ct_filter == "主たる治験"): return 1
+    elif (ct_filter == "拡大治験"): return 2
+    elif (ct_filter == "主たる治験と拡大治験のいずれにも該当しない"): return 3
+
+def process_ct_progression(ct_progression):
+    if (ct_progression == None or ct_progression == ""): return ct_progression
+    if ("募集前" == ct_progression): return 1
+    elif ("募集中" == ct_progression): return 2
+    elif ("募集中断" == ct_progression): return 3
+    elif ("募集終了" == ct_progression): return 4
+    elif ("研究終了" == ct_progression): return 5
+
+def process_ct_type(ct_type):
+    if (ct_type == None or ct_type == ""): return ct_type
+    if (ct_type == "介入研究"): return 1
+    elif (ct_type == "観察研究"): return 2
+
+# ①日付はdate型にする *
+# ②治験名は長い場合があるのでtextに、その他名前とかはｖarchar(255)にする *
+# research_type ③治験のタイプはtiny intにして、１：企業治験、2：医師主導治験にする *
+# ct_filter 1 主たる治験 2 拡大治験 3 主たる治験と拡大治験のいずれにも該当しない *
+# email varchar (255) *
+# num MediumInt *
+# jrct varchar(255) *
+# ct_type 1 介入研究 2 観察研究 *
+# ct progression 試験進捗状況 1募集前　2募集中 3募集中断 4募集終了　5研究終了 *
+# ひとつの治験に対する病院は複数になるので検索を考えて、治験　＞　病院複数でもてるテーブルを別で作る
+
+# https://docs.google.com/spreadsheets/d/1vWqzDrA_jyHXQIi2fQTHK8J6zYC7f8cSCrtBC4Km3OI/edit?usp=sharing
+
+# 病院名は揺らぎがあるので、現在１つになっていると思うが複数（例えば５個ぐらい持てるようにして）
+# スクレイピングで存在しない病院名が発生したら、通知するようにしてその病院名を既存の病院テーブルに追加する。
+# 新しい病院名は当面ＳＱＬで追加するで良いと思います
+
+# エクセル取得は毎日新規でもよい
+# 　インサートとUPDATEはJRCTのIDで判断する必要がある
+
+# 毎回クリアするのではなく、historyは残すように
+
+def process_rows(old_rows):
+    new_rows = []
+
+    for row in old_rows:
+
+        # 多施設のデータはこのテーブルに挿入しない
+        row.pop(6)
+
+        # DATE
+        row[0] = process_date(row[0])
+        row[1] = process_date(row[1])
+        row[2] = process_date(row[2])
+        # research_type
+        row[3] = process_research_type(row[3])
+        # ct_filter
+        row[4] = process_ct_filter(row[4])
+        #ct_progression
+        row[13] = process_ct_progression(row[13])
+        # ct_type
+        row[18] = process_ct_type(row[18])
+        new_rows.append(row)
+
+    return new_rows
+
+rows = process_rows(rows)
+
+# ['初回公表日', '最終公表日', '実施期間（終了日）', '研究の種別', '治験の区分', 
+#  '責任研究者の組織名', '他施設の責任研究者の組織名', '試験のフェーズ', '対象疾患名', '医薬品の一般名称', 
+#  '販売名', '研究資金等の提供組織名称', '依頼者の名称', '他の臨床研究登録期間発行の研究番号', '試験進捗状況',
+#  'JRCT_ID', '研究名称', '組入れ開始日', '試験概要の目標症例数', '試験概要の試験のタイプ', 
+#  '試験問い合わせ窓口のE-mail', '試験問い合わせ窓口の担当者', 'url', '平易な研究名称']
+
+
+names_en = ['date_public_first', 'date_public_final', 'date_end', 'research_type', 'ct_filter',
+           'researcher_org', 'ct_phase', 'disease_name', 'medicine_general_name', 
+           'medicine_brand_name','funding_org', 'dependence_name', 'research_num', 'ct_progression', 
+           'jrctid','research_name', 'enrollment_start_date', 'num_target_disease', 'ct_type', 
+           'inquiry_window_email', 'inquiry_window_person', 'url', 'research_simple_name']
+
+types = ["DATE", "DATE", "DATE", "TEXT", "VARCHAR(255)",
+         "VARCHAR(255)", "VARCHAR(255)", "TEXT", "TEXT",
+         "TEXT", "VARCHAR(255)", "VARCHAR(255)", "VARCHAR(255)", "TEXT",
+         "VARCHAR(255)", "TEXT", "VARCHAR(255)", "MEDIUMINT", "TEXT",
+         "VARCHAR(255)", "VARCHAR(255)", "VARCHAR(255)", "TEXT"]
+
+# 多施設のデータはこのテーブルに挿入しない
+names_jp.pop(6)
 
 row_new = []
 
-for k, v in zip (fieldnames, name_en):
-    row_new.append(k + ':' + v)
+for a, b, c in zip (names_jp, names_en, types):
+    row_new.append(a + ':' + b + ':' + c)
 
 fieldnames = row_new
 
@@ -57,14 +163,17 @@ print("DB connected")
 
 cursor = connection.cursor()
 
-cursor.execute('DROP TABLE IF EXISTS ' + t_name)
-fields_string = ", ".join(list(map(lambda e: e.split(":")[1] + " TEXT COMMENT \'" + e.split(":")[0] + "\'", fieldnames)))
-create_query = "CREATE Table " + t_name + "(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " + fields_string + ")DEFAULT CHARACTER SET=utf8"
+# cursor.execute('DROP TABLE IF EXISTS ' + t_name)
+fields_string = ", ".join(list(map(lambda e: e.split(":")[1] + " " + e.split(":")[2] + " COMMENT \'" + e.split(":")[0] + "\'", fieldnames)))
+create_query = "CREATE TABLE IF NOT EXISTS " + t_name + "(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " + fields_string + ", UNIQUE KEY (jrctid))DEFAULT CHARACTER SET=utf8"
 cursor.execute(create_query)
 
 fields_title_string = ", ".join(list(map(lambda e: e.split(":")[1], fieldnames)))
 
-query = 'INSERT INTO ' + t_name + '(' + fields_title_string + ') VALUES '+ "(" + '%s, ' * (len(fieldnames) - 1) + '%s' + ")"                                                         
+update_string = ", ".join(list(map(lambda e: e + "=ins." + e, names_en)))
+print(update_string)
+
+query = 'INSERT INTO ' + t_name + '(' + fields_title_string + ') VALUES '+ "(" + '%s, ' * (len(fieldnames) - 1) + '%s' + ")" + " AS ins ON DUPLICATE KEY UPDATE " + update_string                                                         
 
 cursor.executemany(query, rows)
 
